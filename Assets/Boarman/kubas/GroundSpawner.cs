1b106e8e95f91e68a219f95e470cf62c499e252d
Yanderecentaur <146071033+BlebaSbluzu@users.noreply.github.com>
2023-11-26 20:17:20 +0000

    Road Length extend

--- Assets/Boarman/kubas/GroundSpawner.cs
+++ Assets/Boarman/kubas/GroundSpawner.cs
@@ -15,7 +15,7 @@ public class GroundSpawner : MonoBehaviour
 
     private void Start()
     {
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 100; i++) {
             SpawnTile();
         }
            
